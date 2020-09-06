8c638619e82d0bcb271b62f1e33d74e593b7b081
Stefan von Buddenbrock <stefvonb@Stefans-MacBook-Pro.local>
2020-09-06 19:38:06 +0200

    Removed env.sh

--- env.sh
+++
@@ -1,4 +0,0 @@
-export PATH=/Users/stefvonb/Projects/MadMax/.env/bin:$PATH
-export LD_LIBRARY_PATH=/Users/stefvonb/Projects/MadMax/.env/lib:$LD_LIBRARY_PATH
-export DYLD_LIBRARY_PATH=/Users/stefvonb/Projects/MadMax/.env/lib:$DYLD_LIBRARY_PATH
-export PYTHONPATH=/Users/stefvonb/Projects/MadMax/.env//Users/stefvonb/Projects/MadMax/.env/lib/python3.7/site-packages:$PYTHONPATH

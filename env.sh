378e8b5b8f41d7b65814b59de57b1a76f7b1baa5
Stefan von Buddenbrock <stefvonb@Stefans-MacBook-Pro.local>
2020-09-06 19:36:13 +0200

    Cleaned up install dependencies

---
+++ env.sh
@@ -0,0 +1,4 @@
+export PATH=/Users/stefvonb/Projects/MadMax/.env/bin:$PATH
+export LD_LIBRARY_PATH=/Users/stefvonb/Projects/MadMax/.env/lib:$LD_LIBRARY_PATH
+export DYLD_LIBRARY_PATH=/Users/stefvonb/Projects/MadMax/.env/lib:$DYLD_LIBRARY_PATH
+export PYTHONPATH=/Users/stefvonb/Projects/MadMax/.env//Users/stefvonb/Projects/MadMax/.env/lib/python3.7/site-packages:$PYTHONPATH
